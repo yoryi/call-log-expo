c0262996f6723b6d2166967c37caa368b9e7255e
yoryi <yoryiojeda@outlook.com>
2024-07-22 17:07:47 -0500

    feat: view integration

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as moduleNative from './modules/callregister';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const handleMakeCall = () => {
+    const actions = moduleNative.hello();
+    console.log('respuesta: ', actions);
+  }
+
+  const renderUI = () => {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text style={{fontWeight: 'bold', fontSize: 15}}>App Prueba para llamadas!</Text>
+        <TouchableOpacity onPress={handleMakeCall} style={{backgroundColor: 'black', padding: 20, borderRadius: 20, marginTop: 40}}>
+          <Text style={{color: 'white'}}>Realizar llamada a: +57-300-929-****</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  return renderUI();
 }
 
 const styles = StyleSheet.create({
